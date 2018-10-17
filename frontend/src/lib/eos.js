4fa53b8ef0fe67de1258e492a0ee0ddc7e55c608
fabifrank <info@fabifrank.com>
2018-10-17 17:37:42 +0200

    file purchase blockchain transaction process

--- frontend/src/lib/eos.js
+++ frontend/src/lib/eos.js
@@ -9,6 +9,7 @@ export function generateUuid() {
   return uuidv5()
 }
 
+
 export class Eos {
   constructor(key) {
     console.log('key', key)
@@ -49,6 +50,97 @@ export class Eos {
       console.error('Cannot retreive active nodes: ', err)
     })
   }
+
+  getPurchasedFiles() {
+    return this.client.getTableRows({json:true, scope: config.contract, code: config.contract,  table: 'files', limit:100})
+    .then((res) => {
+      console.log('eos.getFiles', res)
+      return res.rows
+    }).catch((err) => {
+      console.error('Cannot retreive active nodes: ', err)
+    })
+  }
+
+  buy(sender, quantity, uuid) {
+    const self = this
+    return self.prepare(sender)
+    .then(() => self.transfer(sender, config.contract, quantity))
+    .then(() => self.purchase(sender, uuid))
+    .then(() => {
+      console.log('successfully purchased file')
+    })
+    .catch((err) => {
+      console.error('error while purchasing file', err)
+    })
+  }
+
+  prepare(user) {
+    console.log('prepare', user)
+    return this.client.transaction(
+      {
+        actions: [
+          {
+            account: config.contract,
+            name: 'prepare',
+            authorization: [{
+              actor: user,
+              permission: 'active',
+            }],
+            data: {
+              user: user
+            }
+          }
+        ]
+      }
+    )
+  }
+
+
+  transfer(sender, receiver, quantity) {
+    return this.client.transaction(
+      {
+        actions: [
+          {
+            account: 'eosio.token',
+            name: 'transfer',
+            authorization: [{
+              actor: sender,
+              permission: 'active',
+            }],
+            data: {
+              from: sender,
+              to: receiver,
+              quantity: quantity,
+              memo: ''
+            }
+          }
+        ]
+      }
+    )
+  }
+
+
+  purchase(user, uuid) {
+    return this.client.transaction(
+      {
+        actions: [
+          {
+            account: config.contract,
+            name: 'purchase',
+            authorization: [{
+              actor: user,
+              permission: 'active',
+            }],
+            data: {
+              buyer: user,
+              uuid: uuid
+            }
+          }
+        ]
+      }
+    )
+  }
+
 }
 
 
--- frontend/src/organisms/file.js
+++ frontend/src/organisms/file.js
@@ -11,11 +11,21 @@ class File extends Component {
     this.state = {}
     console.log('Load uuid', this.props.match.params.uuid)
     this.props.loadFiles()
+    this.purchase = this.purchase.bind(this)
+    this.getItem = this.getItem.bind(this)
   }
 
+  purchase() {
+    const item = this.getItem()
+    this.props.auth.eos.buy(this.props.auth.user, item.price, item.uuid)
+  }
+
+  getItem() {
+    return this.props.files && this.props.files.items && this.props.files.items.find((x) => x.uuid == this.props.match.params.uuid)
+  }
 
   render() {
-    const item = this.props.files && this.props.files.items && this.props.files.items.find((x) => x.uuid == this.props.match.params.uuid)
+    const item = this.getItem()
     if (!item) {
       return (
         <div>This file does not exist {this.props.match.params.uuid}</div>
@@ -50,6 +60,10 @@ class File extends Component {
         <div className="row">
           <div className="col-md-2">Owner</div>
           <div className="col-md-10">{item.owner}</div>
+        </div>
+        <br/><br/>
+        <div className="row">
+          <div className="col-md-12"><button className="btn btn-success" onClick={this.purchase}>Download for {item.price}</button></div>
         </div>
       </div>
     );
