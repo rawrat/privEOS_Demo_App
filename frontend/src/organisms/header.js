a3638c5cf4be210b53d5e61a8df7ef1f8087dee2
fabifrank <info@fabifrank.com>
2018-10-23 17:59:37 +0200

    style logout button

--- frontend/src/organisms/header.js
+++ frontend/src/organisms/header.js
@@ -12,8 +12,7 @@ class Header extends Component {
             <li className="width10"><a href="https://slant.li/priveos" target="_blank"><img src="/img/dark_logo_transparent.png" width="100%" /></a></li>
             <li><NavLink to="/" exact="true" activeClassName="active">Files</NavLink></li>
             <li><NavLink to="/upload" exact="true" activeClassName="active">Upload</NavLink></li>
-            <li><NavLink to="/logout" exact="true" activeClassName="active">Logout</NavLink></li>
-            <li className="smallFont">Logged in as <strong>{this.props.auth.account.name}</strong></li>
+            <li className="smallFont">Logged in as <strong>{this.props.auth.account.name} <NavLink to="/logout" exact="true" activeClassName="active" className="logout danger">Logout</NavLink></strong></li>
           </ul>
         ) : ( <ul className="mainNavigation"><li></li></ul> )}
       </div>
