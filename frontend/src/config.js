187de98dba80ba646f3e3a5f328ead31306ea2f1
Angelo Laub <angelolaub@gmail.com>
2018-10-31 14:25:13 +0100

    cleanup config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -25,20 +25,9 @@ export default {
     port: port,
     chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
     maxChunkLength: '1024',
-    eosAccounts: [
-        {
-            name: "alice",
-            privateKey: "5HrReeu6FhGFWiHW7tsvLLN4dm2TDhizP9B7xWi4emG9RmVfLss",
-            publicKey: "EOS6Zy532Rgkuo1SKjYbPxLKYs8o2sEzLApNu8Ph66ysjstARrnHm"
-        },
-        {
-            name: "bob",
-            privateKey: "5JqvAdD1vQG3MRAsC9RVzdPJxnUCBNVfvRhL7ZmQ7rCqUoMGrnw",
-            publicKey: "EOS87xyhE6czLCpuF8PaEGc3UiXHHyCMQB2zHygpEsXyDJHadHWFK"
-        }
-    ],
     priveos: {
         dappContract: 'demoapp',
+        brokerUrl: 'http://localhost:4000'
         // privateKey: '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3',
     },
     ...additionalConfig,
